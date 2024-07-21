8cfa253ba0406f189d5c7201bbd3004727589a39
subral jaiswal <76086829+subral@users.noreply.github.com>
2024-07-21 12:14:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '/three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
